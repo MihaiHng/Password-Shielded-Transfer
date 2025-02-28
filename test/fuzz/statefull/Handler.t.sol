ea6d827301122ac91bda0d1d0168b0728309bfb3
mihai <mihai.hanga@gmail.com>
2025-02-28 11:58:33 +0200

    First Commit

--- test/fuzz/statefull/Handler.t.sol
+++ test/fuzz/statefull/Handler.t.sol
@@ -3,17 +3,30 @@
 pragma solidity ^0.8.28;
 
 import {Test, console, console2} from "forge-std/Test.sol";
+import {StdUtils} from "forge-std/StdUtils.sol";
 import {PST} from "src/PST.sol";
 
 contract Handler {
     PST public pst;
 
+    uint256 MAX_AMOUNT_TO_SEND = type(uint96).max;
+    uint256 MIN_AMOUNT_TO_SEND = 1e14;
+
     constructor(PST _pst) {
         pst = _pst;
     }
 
-    function createTransfer(uint256 amount, address receiver) public {
-        //pst.createTransfer{value: amount}(receiver);
+    function createTransfer(address receiver, address token, uint256 amount, string memory password) public {
+        require(receiver != address(0), "Invalid receiver: zero address");
+        require(receiver != msg.sender, "Invalid receiver: cannot be sender");
+
+        require(token != address(0), "Invalid token: zero address");
+
+        amount = bound(amount, MIN_AMOUNT_TO_SEND, MAX_AMOUNT_TO_SEND);
+
+        require(bytes(password).length >= 7, "Password length must be at least 7");
+
+        pst.createTransfer(receiver, token, amount, password);
     }
 
     function cancelTransfer(uint256 transferId) public {
