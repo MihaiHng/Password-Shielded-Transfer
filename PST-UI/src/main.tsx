4980823ed48950254c89a4f9021fb97b790b812c
Mihai <mihai.hanga@gmail.com>
2025-06-06 21:02:02 +0300

    UI

--- PST-UI/src/main.tsx
+++ PST-UI/src/main.tsx
@@ -1,41 +1,21 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
-// import { WagmiProvider } from 'wagmi'
-// import { wagmiConfig } from './web3/web3modal'
-
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <WagmiProvider config={wagmiConfig}>
-//       <App />
-//     </WagmiProvider>
-//   </StrictMode>,
-// )
-
-// src/main.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
-import { AppKitProvider } from '@reown/appkit';
-import { appKitConfig } from './lib/appkit';
-
-const queryClient = new QueryClient();
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { WagmiProvider } from 'wagmi'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { wagmiAdapter, queryClient } from './lib/appkit'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <WagmiProvider config={appKitConfig.wagmiConfig}>
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <AppKitProvider config={appKitConfig}>
-          <App />
-        </AppKitProvider>
+        <App />
       </QueryClientProvider>
     </WagmiProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
+
 
 
 
